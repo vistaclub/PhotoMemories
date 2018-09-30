f6d1b2c4cf5fc5af8172ad9ee068271f50e5b6f5
Jason Wong <jawong71@gmail.com>
2018-09-29 18:49:20 -0700

    first commit

---
+++ Project7/Photo.swift
@@ -0,0 +1,18 @@
+//
+//  Photo.swift
+//  Project7
+//
+//  Created by Jason Wong on 2018-08-27.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+class Photo: NSCollectionViewItem {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
